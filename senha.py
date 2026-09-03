2fe7473eb306b1a2e6d5de8887faa888764b4b1a
gabriel <04539884077@senacrs.edu.br>
2026-09-03 18:46:35 -0300

    Melhoria do sistema de senhas

--- senha.py
+++ senha.py
@@ -1,7 +1,21 @@
+tentativas = 0
+limite = 4
+
 senha = int(input("Digite sua primeira senha: "))
 senha_colocada = int(input("Digite uma senha: "))
 
 while senha_colocada != senha:
-    senha_colocada = int(input("Senha incorreta, digite a senha correta: "))
+    tentativas += 1
 
-print("Senha correta, bem vindo(a)!!" )
+    if tentativas == limite:
+        print("Tentativa final! Bloqueio automático em caso de erro!")
+
+    if tentativas > limite:
+        print("Conta bloquada. Excedeu o limite de tentativas!")
+        break
+
+    senha_colocada = int(input("Senha incorreta, digite a senha correta: "))
+    
+else:
+    print("Bem vindo(a)!")
+
